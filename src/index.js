d94c0e1a2f7b265ea2694e8a54ec83646afed6f1
Areh Tunde <arehtunde96@gmail.com>
2021-02-07 00:35:04 +0100

    completed task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-const button = document.querySelector('.wrapper_button');
-const body = document.querySelector('body');
-const colorText = document.querySelector('.color-text');
-const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025', '#f0eded'];
+/* eslint-disable no-plusplus */
+const counter = document.querySelector('.main_counter');
+const buttons = document.querySelectorAll('button');
+let count = 0;
 
-const randomNumber = () => Math.floor(Math.random() * colors.length);
+buttons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const style = e.target.classList;
 
-button.addEventListener('click', () => {
-  const randomColor = randomNumber();
-  body.style.backgroundColor = colors[randomColor];
-  colorText.textContent = colors[randomColor];
+    if (style.contains('increase')) {
+      counter.innerHTML = ++count;
+    } else if (style.contains('decrease')) {
+      counter.innerHTML = --count;
+    } else {
+      count = 0;
+      counter.innerHTML = count;
+    }
+
+    if (count > 0) {
+      counter.style.color = 'green';
+    } else if (count < 0) {
+      counter.style.color = 'red';
+    } else {
+      counter.style.color = '#011b33';
+    }
+  });
 });
