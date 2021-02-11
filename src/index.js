70d02c3454d26545b235a77299b8b2f1f152207c
Areh Tunde <arehtunde96@gmail.com>
2021-02-11 02:34:08 +0100

    add more styling

--- src/index.js
+++ src/index.js
@@ -1,46 +1,7 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable import/extensions */
-import reviews from './reviews.js';
+const open = document.querySelector('.open');
+const nav = document.querySelector('.nav');
+const show = document.querySelector('.show');
 
-const name = document.querySelector('.details-name');
-const job = document.querySelector('.details-job');
-const text = document.querySelector('.details-text');
-const img = document.querySelector('img');
-
-const prevBtn = document.querySelector('.left');
-const nextBtn = document.querySelector('.right');
-const randomBtn = document.querySelector('.random');
-let current = 0;
-
-const loadPerson = (index) => {
-  const person = reviews[index];
-  img.src = person.img;
-  name.textContent = person.name;
-  job.textContent = person.job;
-  text.textContent = person.text;
-};
-
-window.addEventListener('DOMContentLoaded', () => loadPerson(current));
-
-randomBtn.addEventListener('click', () => {
-  const random = Math.floor(Math.random() * reviews.length);
-  loadPerson(random);
-});
-
-nextBtn.addEventListener('click', () => {
-  current++;
-
-  if (current > reviews.length - 1) {
-    current = 0;
-  }
-  loadPerson(current);
-});
-
-prevBtn.addEventListener('click', () => {
-  current--;
-
-  if (current < 0) {
-    current = reviews.length - 1;
-  }
-  loadPerson(current);
+open.addEventListener('click', () => {
+  nav.classList.toggle('show');
 });
